6931da0192bde48a31081d1e3a7963b059946726
girotobial <girotobial@gmail.com>
2024-11-22 22:55:43 +0000

    allow module repitions

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,5 +1,6 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![warn(clippy::perf, clippy::pedantic)]
+#![allow(clippy::module_name_repetitions)]
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use std::sync::{Arc, Mutex, RwLock};
